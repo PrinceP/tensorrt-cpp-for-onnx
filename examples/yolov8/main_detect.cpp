ec41995e6e38fe0a6d9d9dd640bdaa14b8b39f2b
Prince <prince@een.com>
2024-03-15 19:36:45 +0530

    yolov8 detect correction

--- examples/yolov8/main_detect.cpp
+++ examples/yolov8/main_detect.cpp
@@ -24,6 +24,11 @@ struct PreParam
 	float width = 0;
 };
 
+int num_labels = 80;
+int topk = 100;
+float score_thres = 0.25f;
+float iou_thres = 0.65f;
+
 const std::vector<std::string> CLASS_NAMES = {
         "person",         "bicycle",    "car",           "motorcycle",    "airplane",     "bus",           "train",
         "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",    "parking meter", "bench",
@@ -39,33 +44,28 @@ const std::vector<std::string> CLASS_NAMES = {
         "teddy bear",     "hair drier", "toothbrush"
     };
 
-    const std::vector<std::vector<unsigned int>> COLORS = {
-        {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
-        {162, 20, 47},   {76, 76, 76},    {153, 153, 153}, {255, 0, 0},     {255, 128, 0},   {191, 191, 0},
-        {0, 255, 0},     {0, 0, 255},     {170, 0, 255},   {85, 85, 0},     {85, 170, 0},    {85, 255, 0},
-        {170, 85, 0},    {170, 170, 0},   {170, 255, 0},   {255, 85, 0},    {255, 170, 0},   {255, 255, 0},
-        {0, 85, 128},    {0, 170, 128},   {0, 255, 128},   {85, 0, 128},    {85, 85, 128},   {85, 170, 128},
-        {85, 255, 128},  {170, 0, 128},   {170, 85, 128},  {170, 170, 128}, {170, 255, 128}, {255, 0, 128},
-        {255, 85, 128},  {255, 170, 128}, {255, 255, 128}, {0, 85, 255},    {0, 170, 255},   {0, 255, 255},
-        {85, 0, 255},    {85, 85, 255},   {85, 170, 255},  {85, 255, 255},  {170, 0, 255},   {170, 85, 255},
-        {170, 170, 255}, {170, 255, 255}, {255, 0, 255},   {255, 85, 255},  {255, 170, 255}, {85, 0, 0},
-        {128, 0, 0},     {170, 0, 0},     {212, 0, 0},     {255, 0, 0},     {0, 43, 0},      {0, 85, 0},
-        {0, 128, 0},     {0, 170, 0},     {0, 212, 0},     {0, 255, 0},     {0, 0, 43},      {0, 0, 85},
-        {0, 0, 128},     {0, 0, 170},     {0, 0, 212},     {0, 0, 255},     {0, 0, 0},       {36, 36, 36},
-        {73, 73, 73},    {109, 109, 109}, {146, 146, 146}, {182, 182, 182}, {219, 219, 219}, {0, 114, 189},
-        {80, 183, 189},  {128, 128, 0}
-    };
+const std::vector<std::vector<unsigned int>> COLORS = {
+    {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
+    {162, 20, 47},   {76, 76, 76},    {153, 153, 153}, {255, 0, 0},     {255, 128, 0},   {191, 191, 0},
+    {0, 255, 0},     {0, 0, 255},     {170, 0, 255},   {85, 85, 0},     {85, 170, 0},    {85, 255, 0},
+    {170, 85, 0},    {170, 170, 0},   {170, 255, 0},   {255, 85, 0},    {255, 170, 0},   {255, 255, 0},
+    {0, 85, 128},    {0, 170, 128},   {0, 255, 128},   {85, 0, 128},    {85, 85, 128},   {85, 170, 128},
+    {85, 255, 128},  {170, 0, 128},   {170, 85, 128},  {170, 170, 128}, {170, 255, 128}, {255, 0, 128},
+    {255, 85, 128},  {255, 170, 128}, {255, 255, 128}, {0, 85, 255},    {0, 170, 255},   {0, 255, 255},
+    {85, 0, 255},    {85, 85, 255},   {85, 170, 255},  {85, 255, 255},  {170, 0, 255},   {170, 85, 255},
+    {170, 170, 255}, {170, 255, 255}, {255, 0, 255},   {255, 85, 255},  {255, 170, 255}, {85, 0, 0},
+    {128, 0, 0},     {170, 0, 0},     {212, 0, 0},     {255, 0, 0},     {0, 43, 0},      {0, 85, 0},
+    {0, 128, 0},     {0, 170, 0},     {0, 212, 0},     {0, 255, 0},     {0, 0, 43},      {0, 0, 85},
+    {0, 0, 128},     {0, 0, 170},     {0, 0, 212},     {0, 0, 255},     {0, 0, 0},       {36, 36, 36},
+    {73, 73, 73},    {109, 109, 109}, {146, 146, 146}, {182, 182, 182}, {219, 219, 219}, {0, 114, 189},
+    {80, 183, 189},  {128, 128, 0}
+};
     
 
 void postprocess(std::vector<Object>& objs, float* host_ptrs[1], PreParam pparam, int index, int num_channels, int num_anchors)
 {
 	objs.clear();
     
-    int num_labels = 80;
-    int topk = 100;
-    float score_thres = 0.25f;
-    float iou_thres = 0.65f;
-
     std::vector<cv::Rect> bboxes;
     std::vector<float>    scores;
     std::vector<int>      labels;
@@ -112,7 +112,6 @@ void postprocess(std::vector<Object>& objs, float* host_ptrs[1], PreParam pparam
         }
     }
     cv::dnn::NMSBoxes(bboxes, scores, score_thres, iou_thres, indices);
-    // NMSBoxesBatched(bboxes, scores, labels, score_thres, iou_thres, indices);
     
     int cnt = 0;
     for (auto& i : indices) {
@@ -198,7 +197,7 @@ void draw_objects(
 
 #define MAX_IMAGE_INPUT_SIZE_THRESH 3000 * 3000
 
-std::shared_ptr<spdlog::logger> logger = spdlog::stdout_color_mt("yolov9");
+std::shared_ptr<spdlog::logger> logger = spdlog::stdout_color_mt("yolov8-detect");
 
 class my_formatter_flag : public spdlog::custom_flag_formatter
 {
@@ -455,7 +454,7 @@ int main(int argc, char *argv[]){
             draw_objects(img_batch[index], res, objs, CLASS_NAMES, COLORS);
 
             // Save image
-            cv::imwrite("/app/results/v8_" + img_name_batch[index], res);
+            cv::imwrite("/app/results/v8test_" + img_name_batch[index], res);
             
         }
         SPDLOG_LOGGER_INFO(logger, "Calling postprocess done");
